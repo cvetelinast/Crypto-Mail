aee241b7ea477881f8e1d3e88fd426fc8087d0e3
cvetelinast <cvetelinast.96@abv.bg>
2018-02-04 02:16:26 +0200

    Added popups for wrong user input in LoginComponent. Reordered functions and formatted files.

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -7,33 +7,33 @@ import { Router, ActivatedRoute } from '@angular/router';
 import User = require('../classes/user');
 import 'rxjs/add/operator/map';
 
-@Injectable() 
+@Injectable()
 class RequestsService {
   apiURL: string = 'http://localhost:3000/api/users';
   results: User[];
   headers: Headers;
   options: RequestOptions;
 
-  constructor(private http:Http) { 
+  constructor(private http: Http) {
     this.results = [];
 
-    this.headers = new Headers({ 'Content-Type': 'application/json', 
-                          'Accept': 'q=0.8;application/json;q=0.9' });
+    this.headers = new Headers({
+      'Content-Type': 'application/json',
+      'Accept': 'q=0.8;application/json;q=0.9'
+    });
     this.options = new RequestOptions({ headers: this.headers });
   }
 
   getUsers(): Observable<User[]> {
-    return this.http.get(this.apiURL).map(res => { 
-          return JSON.parse(res.text());
+    return this.http.get(this.apiURL).map(res => {
+      return JSON.parse(res.text());
     });
   }
 
-postUser(user: User): Observable<any> {
-  let body = JSON.stringify(user);
-  console.log(body);
-  debugger;
-  return this.http.post(this.apiURL, body, this.options);
-}
+  postUser(user: User): Observable<any> {
+    let body = JSON.stringify(user);
+    return this.http.post(this.apiURL, body, this.options);
+  }
 
 }
 
@@ -47,6 +47,7 @@ postUser(user: User): Observable<any> {
 export class LoginComponent implements OnInit {
   username: string = '';
   password: string = '';
+  repeatPassword: string = '';
   email: string = '';
   users: User[] = [];
   private loading: boolean = false;
@@ -57,83 +58,124 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     this.loginTab();
   }
-  loginTab(){
-    console.log("login");
-    this.hideRegisterTab();
-    const log = document.getElementsByClassName('login');
-    if (log.length > 0){
-      log[0].setAttribute("style","visibility: visible");
-    }
-  }
-  hideLoginTab(){
-    const hidelog = document.getElementsByClassName('login');
-    if (hidelog.length > 0){
-      hidelog[0].setAttribute("style","visibility: hidden");
-    }
-  }
-  registerTab(){
-    console.log("register");
-    this.hideLoginTab();
-    const reg = document.getElementsByClassName('register');
-    if (reg.length > 0){
-      reg[0].setAttribute("style","visibility: visible");
-    }
-  }
-  hideRegisterTab(){
-    const hidereg = document.getElementsByClassName('register');
-    if (hidereg.length > 0){
-      hidereg[0].setAttribute("style","visibility: hidden");
-    }
-  }
 
-  testRouting(user : User){
+  navigateToMainComponent(user: User) {
     let id = user._id;
     let email = user.email;
     let name = user.username;
-    this.router.navigate(['main'], { queryParams: { userId : id , userEmail: email, username: name }, skipLocationChange: true });
+    this.router.navigate(['main'], { queryParams: { userId: id, userEmail: email, username: name }, skipLocationChange: true });
   }
 
-  login(){
-    let item = new User(this.username, this.password, "", "");
-    this.findUserAndLogIn();
+  login() {
+    if (this.validateUsername() && this.validatePassword()) {
+      let item = new User(this.username, this.password, '', '');
+      this.findUserAndLogIn();
+    }
   }
 
-  register(){
-
-     if(this.password !== document.getElementsByTagName("input")[4].value) {
-       console.log("Wrong password");
-       return;
-     }
-     let item = new User(this.username, this.password, this.email, "");
-     this.findExistingUserOrRegister();
+  register() {
+    if (this.validateUsername() && this.validatePassword()
+      && this.validateSecondPassword() && this.validateEmail()) {
+      let item = new User(this.username, this.password, this.email, '');
+      this.findExistingUserOrRegister();
+    }
   }
 
-findExistingUserOrRegister() {
+  findExistingUserOrRegister() {
     this.loading = true;
-    this.requestsService.getUsers().subscribe( usersList => {
-      for(let u of usersList){
-        if(u.username === this.username
-          || u.password === this.password){
-            return;
+    this.requestsService.getUsers().subscribe(usersList => {
+      for (let u of usersList) {
+        if (u.username === this.username
+          || u.password === this.password) {
+          return;
         }
       }
     }); // .unsubscribe();
-    let user = new User(this.username, this.password, this.email, "");
-    this.requestsService.postUser(user).subscribe( data => {
+    let user = new User(this.username, this.password, this.email, '');
+    this.requestsService.postUser(user).subscribe(data => {
       this.loading = false;
     });;
   }
 
   findUserAndLogIn() {
     this.loading = true;
-    this.requestsService.getUsers().subscribe( usersList => {
-      for(let u of usersList){
-        if(u.username === this.username
-          && u.password === this.password){
-            this.testRouting(u);
+    this.requestsService.getUsers().subscribe(usersList => {
+      for (let u of usersList) {
+        if (u.username === this.username
+          && u.password === this.password) {
+          this.navigateToMainComponent(u);
         }
       }
     }); // .unsubscribe();
   }
+
+  validateUsername(): boolean {
+    if (this.username.length === 0 || this.username.indexOf(' ') >= 0) {
+      const popupName = document.getElementById('myPopupName');
+      popupName.classList.toggle('show');
+      setTimeout(function () { popupName.classList.toggle('show'); }, 2000);
+      return false;
+    }
+    return true;
+  }
+  validatePassword(): boolean {
+    if (this.password.length === 0) {
+      const popupPass = document.getElementById('myPopupPass');
+      popupPass.classList.toggle('show');
+      setTimeout(function () { popupPass.classList.toggle('show'); }, 2000);
+      return false;
+    }
+    return true;
+  }
+  validateSecondPassword(): boolean {
+    if (this.password !== this.repeatPassword) {
+      const popupPassRepeat = document.getElementById('myPopupPassRepeat');
+      popupPassRepeat.classList.toggle('show');
+      setTimeout(function () { popupPassRepeat.classList.toggle('show'); }, 2000);
+      return false;
+    }
+    return true;
+  }
+  validateEmail(): boolean {
+    const re = /\S+@\S+\.\S+/;
+    if (this.email.length === 0 || !re.test(this.email)) {
+      const popupEmail = document.getElementById('myPopupEmail');
+      popupEmail.classList.toggle('show');
+      setTimeout(function () { popupEmail.classList.toggle('show'); }, 2000);
+      return false;
+    }
+    return true;
+  }
+
+  loginTab() {
+    console.log('login');
+    this.hideRegisterTab();
+    const log = document.getElementsByClassName('login');
+    if (log.length > 0) {
+      log[0].setAttribute('style', 'visibility: visible');
+    }
+  }
+  hideLoginTab() {
+    const hidelog = document.getElementsByClassName('login');
+    if (hidelog.length > 0) {
+      hidelog[0].setAttribute('style', 'visibility: hidden');
+    }
+  }
+  registerTab() {
+    console.log('register');
+    this.hideLoginTab();
+    const reg = document.getElementsByClassName('register');
+    if (reg.length > 0) {
+      reg[0].setAttribute('style', 'visibility: visible');
+    }
+  }
+  hideRegisterTab() {
+    const hidereg = document.getElementsByClassName('register');
+    if (hidereg.length > 0) {
+      hidereg[0].setAttribute('style', 'visibility: hidden');
+    }
+  }
+
+  // cool sidenavigation: https://www.w3schools.com/howto/howto_css_sidenav_buttons.asp
 
 }
